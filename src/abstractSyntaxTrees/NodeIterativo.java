740e3b8dced8cbff71f7a399d357efd49fdda223
p4ul0plr <pauloroberto_nobrega@hotmail.com>
2019-02-01 18:38:46 -0300

    Adicionado classes node

---
+++ src/abstractSyntaxTrees/NodeIterativo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractSyntaxTrees;
+
+/**
+ *
+ * @author paulo
+ */
+public class NodeIterativo extends NodeComando {
+    NodeExpressao nodeExpressao;
+    NodeComando nodeComando;
+}
---
+++ src/abstractSyntaxTrees/NodeTipoAgregado.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractSyntaxTrees;
+
+/**
+ *
+ * @author paulo
+ */
+public class NodeTipoAgregado extends NodeTipo {
+    NodeLiteral nodeLiteral1, nodeLiteral2;
+    NodeTipo nodeTipo;
+}
